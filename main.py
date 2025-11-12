cd6966c09241e83fb3283c82da667fe797bf74e9
xkarina2197 <xkarina2197@gmail.com>
2025-11-12 17:52:40 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+# 1-misol
+ism = input("ism kirit:")
+print(ism.upper())
+
+# 2-misol
+matn  = input("matn kirit: ")
+print(matn.lower())
+
+
+# 3-misol
+parol = input("parol kirit: ")
+if parol == "admin":
+    print("xavfsiz parol")
+else:
+    print("xavfli parol")
+
+# 4-misol
+matn = input("matn kirit: ")
+print(matn.strip())
+
+# 5-misol
+matn = input("email manzilni kirit: ")
+print(matn.strip())
+
+# 6-misol
+matn = ("  Hello world  ")
+print(matn.lstrip())
+
+# 7-misol
+matn = "  Hello my best friend  "
+print(matn.rstrip())
